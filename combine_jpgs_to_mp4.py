cde811c54945ea9b91601e72459115795df50abe
yang27Angela <103293127+yang27Angela@users.noreply.github.com>
2022-06-23 23:08:26 -0400

    fixed minor bugs

--- combine_jpgs_to_mp4.py
+++ combine_jpgs_to_mp4.py
@@ -2,26 +2,35 @@ import cv2
 import numpy as np
 import glob, os
 
+import time
+import sys
+from stat import S_ISREG, ST_CTIME, ST_MODE
 # /Users/angelayang/APS360/Video Sampling/combine_jpgs_to_mp4.py
 
 img_array = []
-path = "/Users/angelayang/APS360/Video Sampling/"
-# arr = os.listdir("/Users/angelayang/APS360/Video Sampling/")
-# print(arr)
 
-filenames = next(os.walk("/Users/angelayang/APS360/Video Sampling/"), (None, None, []))[2]
-filenames = [filename for filename in filenames if '.jpg' in filename]
-print(len(filenames))
+#Relative or absolute path to the directory
+dir_path = sys.argv[1] if len(sys.argv) == 2 else r'.'
 
-for filename in filenames:
-    # print(filename)
+#all entries in the directory w/ stats
+data = (os.path.join(dir_path, fn) for fn in os.listdir(dir_path))
+data = ((os.stat(path), path) for path in data)
 
-    img = cv2.imread(path+filename)
-    height, width, layers = img.shape
-    size = (width,height)
-    print(width, height)
+# regular files, insert creation date
+data = ((stat[ST_CTIME], path)
+           for stat, path in data if S_ISREG(stat[ST_MODE]))
 
-    img_array.append(img)
+
+for cdate, path in sorted(data):
+    filename=os.path.basename(path)
+    print(filename)
+    if '.jpg' in filename:
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        print(filename)
+        img_array.append(img)
+
 
 out = cv2.VideoWriter('new_video.mp4',cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 20, (1920 ,1080))
 for i in range(len(img_array)):
